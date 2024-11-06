8bfc08cc2007a6478dbc400ae49aa647a7ac2b67
= <naufalsalman1810.gmail.com>
2024-11-06 23:30:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recycleview_galery/Gallery.kt
@@ -0,0 +1,3 @@
+package com.example.recycleview_galery
+
+data class Gallery( val nameGallery: String="", val gallerySource: Int)
---
+++ app/src/main/java/com/example/recycleview_galery/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.recycleview_galery
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.receycleview_galery.R
+import com.example.receycleview_galery.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        val adapterGallery = GalleryAdapter(generatedataDummy())
+        with(binding){
+            recyclerViewGallery.apply {
+                adapter = adapterGallery
+                layoutManager = GridLayoutManager(this@MainActivity, 3)
+            }
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun generatedataDummy(): List<Gallery>{
+        return listOf(
+            Gallery("Gallery 1", R.drawable.galeri1),
+            Gallery("Gallery 2", R.drawable.galeri2),
+            Gallery("Gallery 3", R.drawable.galeri3),
+            Gallery("Gallery 4", R.drawable.galeri4),
+            Gallery("Gallery 5", R.drawable.galeri5),
+            Gallery("Gallery 6", R.drawable.galeri6),
+            Gallery("Gallery 7", R.drawable.galeri7),
+        )
+    }
+}
